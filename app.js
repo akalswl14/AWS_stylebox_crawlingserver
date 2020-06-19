221e8aa7c17191fbd46fbe90f829ca819e809328
Minji Ma <gnkryd12@gmail.com>
2020-06-19 15:55:19 +0900

    fix response header

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://ec2-54-255-199-236.ap-southeast-1.compute.amazonaws.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', '*');
     next();
   });
 app.use(express.static('public'))
