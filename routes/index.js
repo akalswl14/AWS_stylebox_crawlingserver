98f593221debdb28bc22fe6d21aa863401a25409
Minji Ma <gnkryd12@gmail.com>
2020-06-23 13:15:42 +0900

    for test

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,8 @@ router.get('/firstcrawling', function (req, res) {
 router.post('/downloadfeed', async function (req, res) {
     console.log('downloading feed!');
     console.log(req.body);
+    downloadzip(res);
+    return;
     let dbData = await checkLastUpdateDateTable();
     if (dbData.Item.crawlingstatus == true) {
         res.redirect('http://ec2-54-255-199-236.ap-southeast-1.compute.amazonaws.com:8080/');
@@ -37,3 +39,41 @@ async function checkLastUpdateDateTable() {
     }
 }
 module.exports = router;
+
+async function downloadzip(res) {
+    var fs = require('fs')
+    const join = require('path').join
+    const s3Zip = require('s3-zip')
+    const XmlStream = require('xml-stream')
+
+    const bucket = 'downloaddata-stylebox'
+    const folder = '/'
+    const params = {
+        Bucket: bucket,
+        Prefix: folder
+    }
+
+    const filesArray = []
+    const files = s3.listObjects(params).createReadStream()
+    const xml = new XmlStream(files)
+    xml.collect('Key')
+    xml.on('endElement: Key', function (item) {
+        filesArray.push(item['$text'].substr(folder.length))
+    })
+
+    xml
+        .on('end', function () {
+            zip(filesArray,res)
+        })
+
+    function zip(files,res) {
+        console.log(files)
+        const output = fs.createWriteStream(join(__dirname, 'use-s3-zip.zip'))
+        res.set('Content-disposition', 'attachment; filename=' + 'DownloadData.zip');
+        // res.set('Content-Type', 'application/octet-stream');
+        res.set('content-type', 'application/zip')
+        s3Zip
+            .archive({ bucket: bucket, preserveFolderStructure: true }, folder, files)
+            .pipe(res)
+    }
+}
