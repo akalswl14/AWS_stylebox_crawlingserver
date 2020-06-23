b6f87840171b7998c9ddd7251c6aa93a9d5d96ff
Minji Ma <gnkryd12@gmail.com>
2020-06-23 15:45:38 +0900

    delete test code

--- routes/index.js
+++ routes/index.js
@@ -21,8 +21,6 @@ router.get('/firstcrawling', function (req, res) {
 router.post('/downloadfeed', async function (req, res) {
     console.log('downloading feed!');
     console.log(req.body);
-    downloadzip(res);
-    return;
     let dbData = await checkLastUpdateDateTable();
     if (dbData.Item.crawlingstatus == true) {
         res.redirect('http://ec2-54-255-199-236.ap-southeast-1.compute.amazonaws.com:8080/');
@@ -45,28 +43,3 @@ async function checkLastUpdateDateTable() {
     }
 }
 module.exports = router;
-
-async function downloadzip(res) {
-    var fs = require('fs')
-    const s3Zip = require('s3-zip')
-    var FileNameList = await getobjectList();
-    res.set('Content-disposition', 'attachment; filename=' + 'DownloadData.zip');
-    // res.set('Content-Type', 'application/octet-stream');
-    res.set('content-type', 'application/zip');
-    s3Zip
-        .archive({ region: 'ap-southeast-1', bucket: 'downloaddata-stylebox' }, '/', FileNameList)
-        .pipe(res)
-}
-async function getobjectList(){
-    var params = {
-        Bucket: "examplebucket",
-        MaxKeys: 2
-    };
-    let data = await s3.listObjectsV2(params).promise();
-    data = data.Contents;
-    var FileNameList = [];
-    for (var i = 0; i < data.length; i++) {
-        FileNameList.push(data[i].Key);
-    }
-    return FileNameList;
-}
